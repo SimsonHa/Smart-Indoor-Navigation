1085998c83019f5841e9fd4251744a3e3c76064b
SimsonHa <shannappel61@gmail.com>
2019-05-10 22:19:54 +0200

    added basic upload

--- Verwaltungsfrontend/html/js/fileupload.js
+++ Verwaltungsfrontend/html/js/fileupload.js
@@ -1,27 +1,17 @@
-function dateiauswahl(evt) {
-    var datei = evt.target.datei; // FileObject
+   function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
 
-      // nur Bild-Dateien
-      if (datei.type.match('image.*')) {
-        continue;
-      }
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
 
-      var reader = new FileReader();
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+     }
 
-      reader.onload = (function(theFile) {
-        return function(e) {
-          // erzeuge Thumbnails.
-          var vorschau = document.createElement('img');
-		  vorschau.className = 'vorschau';
-		  vorschau.src   = e.target.result;
-		  vorschau.title = theFile.name;
-          document.getElementById('list').insertBefore(vorschau, null);
-        };
-      })(f);
-
-      // Bilder als Data URL auslesen.
-      reader.readAsDataURL(f);
-
-  }
-  // Auf neue Auswahl reagieren und gegebenenfalls Funktion dateiauswahl neu ausführen.
-  document.getElementById('file').addEventListener('change', dateiauswahl, false);
+  previewFile();  //calls the function named previewFile()
