361a5a445f1bb6857cc87383e12a32141cb54076
Henry <Henry@DESKTOP-QEI9HLB>
2019-05-11 14:02:17 +0200

    First Commit

---
+++ SmartIndoorNavigation/src/main/java/com/Snoopy/SmartIndoorNavigation/SmartIndoorNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.Snoopy.SmartIndoorNavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIndoorNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIndoorNavigationApplication.class, args);
+	}
+
+}
