6dca80c7a9aced3c7dad3fcaace4ae2b20c35e5f
Henry <Henry@172.16.28.147>
2019-05-13 09:18:04 +0200

    master commit

--- SmartIndoorNavigation/src/main/java/com/Snoopy/SmartIndoorNavigation/Controller/FrontToBackendTest.java
+++ SmartIndoorNavigation/src/main/java/com/Snoopy/SmartIndoorNavigation/Controller/FrontToBackendTest.java
@@ -3,31 +3,22 @@ package com.Snoopy.SmartIndoorNavigation.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Snoopy.SmartIndoorNavigation.Model.Entity.Product;
 import com.Snoopy.SmartIndoorNavigation.Model.Repository.ProductRepository;
 
-@Controller
+@RestController
 public class FrontToBackendTest {
 
 	@Autowired
 	ProductRepository repository;
 	
-	    @GetMapping("/test")
-	    public String test(Model model) {
+	    @GetMapping("/product")
+	    public List<Product> product() {
 	       List<Product> p = (List<Product>) repository.findAll();
-	        	model.addAttribute("products", p);
-	        return "test.html";
-	    }
-	    @GetMapping("/index")
-	    public String index() {
-	        return "index";
-	    }
-	    @GetMapping("/canvas")
-	    public String canvas() {
-	        return "canvas";
+	        return p;
 	    }
 }
